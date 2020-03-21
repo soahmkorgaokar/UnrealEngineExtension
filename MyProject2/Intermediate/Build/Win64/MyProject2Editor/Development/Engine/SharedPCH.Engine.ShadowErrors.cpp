579220a499e81b3f1fce9e03664d48acc7ab6796
Elliot <ejp4604@rit.edu>
2020-03-21 19:12:58 -0400

    NewStyleContentPlugin
    Used something called an 'Editor Utility Widget' which is an easy way to create editor UI elements

--- MyProject2/Intermediate/Build/Win64/MyProject2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ MyProject2/Intermediate/Build/Win64/MyProject2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,1 +1,1 @@
-#include "Z:/IGMProfile/Desktop/MyProject2/Intermediate/Build/Win64/MyProject2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "D:/RepoUnreal/UnrealProjEngines/UnrealExtension_CharacterActors/MyProject2/Intermediate/Build/Win64/MyProject2Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- MyProject2/Intermediate/Build/Win64/MyProject2Editor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
+++ MyProject2/Intermediate/Build/Win64/MyProject2Editor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
@@ -1,1 +1,1 @@
-#include "Z:/IGMProfile/Desktop/MyProject2/Intermediate/Build/Win64/MyProject2Editor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
+#include "D:/RepoUnreal/UnrealProjEngines/UnrealExtension_CharacterActors/MyProject2/Intermediate/Build/Win64/MyProject2Editor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
